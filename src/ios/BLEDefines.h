1de14f770df0453a05958fec66ed65c879c3d07b
Damien Walsh <me@damow.net>
2018-09-06 13:40:51 +0100

    Change: Swap UUIDs to support SerialIO's device

--- src/ios/BLEDefines.h
+++ src/ios/BLEDefines.h
@@ -1,14 +1,14 @@
 
 /*
- 
+
  Copyright (c) 2013 RedBearLab
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
+
  */
 
 // BlueGiga Service
@@ -28,10 +28,10 @@
 #define ADAFRUIT_CHAR_TX_UUID                         "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 #define ADAFRUIT_CHAR_RX_UUID                         "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 
-// Laird Virtual Serial Port (vSP) service for BL600 http://www.lairdtech.com/DownloadAsset.aspx?id=2147489885
-#define LAIRD_SERVICE_UUID                       "569a1101-b87f-490c-92cb-11ba5ea5167c"
-#define LAIRD_CHAR_TX_UUID                       "569a2000-b87f-490c-92cb-11ba5ea5167c"
-#define LAIRD_CHAR_RX_UUID                       "569a2001-b87f-490c-92cb-11ba5ea5167c"
+// SerialIO device, replacing the previous "Laird" brand one
+#define LAIRD_SERVICE_UUID                       "EBBF6B05-83A7-8E6F-F61D-934514078D34"
+#define LAIRD_CHAR_TX_UUID                       "1CCE1EA8-BD34-4813-A00A-C76E028FADCB"
+#define LAIRD_CHAR_RX_UUID                       "CACC07FF-FFFF-4C48-8FAE-A9EF71B75E26"
 
 // HM-10 (unfortunately this is also the UUID for the TI simple key service)
 // http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#Simple_Key_Service
@@ -39,11 +39,6 @@
 #define HM10_CHAR_TX_UUID                       "ffe1"
 #define HM10_CHAR_RX_UUID                       "ffe1"
 
-// HC-02
-// http://www.hc01.com/productdetail?productid=20180314021
-#define HC02_SERVICE_UUID "49535343-FE7D-4AE5-8FA9-9FAFD205E455"
-#define HC02_CHAR_TX_UUID "49535343-1E4D-4BD9-BA61-23C647249616"
-#define HC02_CHAR_RX_UUID "49535343-8841-43F4-A8D4-ECBE34729BB3"
-#define HC02_ADV_UUID "18F0"
 
 #define RBL_BLE_FRAMEWORK_VER                    0x0200
+
